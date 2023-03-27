e9db08a9d2b7e8bb51c189aa22a45046fb82784d
tomjohnneill <tomjohnneill@live.co.uk>
2023-03-27 12:09:53 +0100

    add the emails to step 3

--- components/Card.js
+++ components/Card.js
@@ -17,6 +17,7 @@ const Card = ({ details, clickable, handleCardClick, content, rtl }) => {
 
   const websitesArr = details.Links?.split(",");
   const phonesArr = details.Phones?.split(",");
+  const email = details.Email;
 
   const contents = (
     <>
@@ -172,6 +173,7 @@ const Card = ({ details, clickable, handleCardClick, content, rtl }) => {
                 </a>
               ))}
           </div>
+
           {rtl && (
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -189,6 +191,51 @@ const Card = ({ details, clickable, handleCardClick, content, rtl }) => {
             </svg>
           )}
         </div>
+        {email && email !== "N/A" && (
+          <div className={`mt-4 px-4 flex ${rtl ? "self-end" : ""}`}>
+            {!rtl && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+              </svg>
+            )}
+            <a href={`mailto:${email?.replace("mailto:", "")}`} key={email}>
+              <p
+                className={`${
+                  rtl ? "mr-2 text-right" : "ml-2"
+                } text-blue-700  underline`}
+              >
+                {email.replace("mailto:", "")}
+              </p>
+            </a>
+            {rtl && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+              </svg>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
